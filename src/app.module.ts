3443f09d2f1729c02564b0956a9244a8d0cf6864
Sawai Chungsri <sawaiwai@gmail.com>
2026-08-25 02:39:21 +0700

    feat: integrate Prisma with NestJS, add DatabaseModule and PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, DatabaseModule],
   controllers: [AppController],
 })
 export class AppModule {}
